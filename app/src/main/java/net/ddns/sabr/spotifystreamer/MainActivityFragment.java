50b30b3a15da879cbd1952f5f67e8a692a408e7d
Abdel Abdalla <abdelabdalla@gmail.com>
2015-06-20 13:38:17 +0100

    Some stuff

--- app/src/main/java/net/ddns/sabr/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/net/ddns/sabr/spotifystreamer/MainActivityFragment.java
@@ -3,6 +3,9 @@ package net.ddns.sabr.spotifystreamer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,8 +19,34 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate((savedInstanceState));
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+        if(id == R.id.action_search){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
     }
 }
