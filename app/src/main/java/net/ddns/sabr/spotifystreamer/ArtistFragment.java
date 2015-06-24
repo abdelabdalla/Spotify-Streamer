65755b578cdb84c172c1c52f1373b4d353690553
Abdel Abdalla <abdelabdalla@gmail.com>
2015-06-24 20:20:29 +0100

    Started adding code to get artist image

--- app/src/main/java/net/ddns/sabr/spotifystreamer/ArtistFragment.java
+++ app/src/main/java/net/ddns/sabr/spotifystreamer/ArtistFragment.java
@@ -1,8 +1,12 @@
 package net.ddns.sabr.spotifystreamer;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,7 +14,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,6 +24,7 @@ import java.util.Arrays;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistSimple;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
 
@@ -27,14 +34,16 @@ import kaaes.spotify.webapi.android.models.ArtistsPager;
  */
 public class ArtistFragment extends Fragment {
 
-    private ArrayAdapter<String> adapter;
+    private ArrayAdapter<String> adapterText;
+    private ArrayAdapter<Image> adapterImage;
+
 
     public String artistName = "Ellie Goulding";
     String[] dataArray = {
         "Search for an artist above"
     };
 
-    
+    EditText search;
 
     public ArtistFragment() {
     }
@@ -66,16 +75,42 @@ public class ArtistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        search = (EditText) rootView.findViewById(R.id.artistSearch);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().isEmpty()){
+                    FetchArtistClass fetch = new FetchArtistClass();
+                    fetch.execute(s.toString());
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
         ArrayList<String> fakeData = new ArrayList<>(Arrays.asList(dataArray));
 
-        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_artist, R.id.list_item_artist_textview, fakeData);
+        adapterText = new ArrayAdapter<String>(getActivity(), R.layout.list_item_artist, R.id.artisttextView, fakeData);
+        //adapterImage = new ArrayAdapter<Image>(getActivity(), R.layout.list_item_artist, R.id.artistImage, );
 
         ListView listView = (ListView) rootView.findViewById(R.id.artist_list_view);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapterText);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String t = adapterText.getItem(position);
+                Intent intent = new Intent(getActivity(), Top10Activity.class).putExtra(Intent.EXTRA_TEXT, t);
+                startActivity(intent);
+
+            }
+        });
 
         return rootView;
     }
@@ -84,8 +119,13 @@ public class ArtistFragment extends Fragment {
 
             @Override
             protected String[] doInBackground(String... params) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
-               SpotifyApi api = new SpotifyApi();
+                SpotifyApi api = new SpotifyApi();
 
                 SpotifyService spotify = api.getService();
 
@@ -101,6 +141,9 @@ public class ArtistFragment extends Fragment {
                     i++;
                 }
 
+                fetchimages f = new fetchimages();
+                f.execute(results);
+
                 return artistList;
             }
 
@@ -108,11 +151,27 @@ public class ArtistFragment extends Fragment {
             protected void onPostExecute(String[] strings) {
 
                 if(strings != null){
-                    adapter.clear();
-                    adapter.addAll(strings);
+                    adapterText.clear();
+                    adapterText.addAll(strings);
                 }
 
             }
         }
+
+    public class fetchimages extends AsyncTask<ArtistsPager, Void, String[]>{
+
+        @Override
+        protected String[] doInBackground(ArtistsPager... params) {
+
+            for(Artist a : params[0].artists.items){
+                if(a.images.size() > 0){
+                    Log.v("picArt", a.images.get(0).url);
+                }
+            }
+
+
+            return new String[0];
+        }
+    }
 
 }
