1f6cc01e6e77a032195de77d52564a21b63410ee
Abdel Abdalla <abdelabdalla@gmail.com>
2015-10-06 21:21:00 +0100

    Save

--- app/src/main/java/net/ddns/sabr/spotifystreamer/Top10Activity.java
+++ app/src/main/java/net/ddns/sabr/spotifystreamer/Top10Activity.java
@@ -269,13 +269,8 @@ public class Top10Activity extends ActionBarActivity {
             return (Application) method.invoke(null, (Object[]) null);
         } catch (final ClassNotFoundException e) {
             // handle exception
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) {
+            e.printStackTrace();}
         return null;
     }
 
