88fdcc228e5809809bb51000ef1f38c97113b399
Abdel Abdalla <abdelabdalla@gmail.com>
2015-10-08 22:05:37 +0100

    Finished player

--- app/src/main/java/net/ddns/sabr/spotifystreamer/PlayerDialogue.java
+++ app/src/main/java/net/ddns/sabr/spotifystreamer/PlayerDialogue.java
@@ -145,7 +145,7 @@ public class PlayerDialogue extends DialogFragment {
                 if(pos == nameA.length - 1){
                     pos = 0;
                 } else {
-                    pos--;
+                    pos++;
                 }
                 mediaPlayer.stop();
                 mediaPlayer.release();
